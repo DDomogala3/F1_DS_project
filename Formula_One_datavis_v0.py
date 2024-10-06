76d5e84a041341358f5cffb4f4e0655ad595c66a
DDomogala3 <ddd38@nau.edu>
2024-10-05 20:12:24 -0500

    Update Formula_One_datavis_v0.py
    st.wrote

--- Formula_One_datavis_v0.py
+++ Formula_One_datavis_v0.py
@@ -10,24 +10,32 @@ import seaborn as sns
 st.write("Formula One Data Analysis:Coming July 2025")
 
 
-st.dataframe(df_cr = pd.read_csv('constructor_results.csv'))
-st.dataframe(df_cs = pd.read_csv('constructor_standings.csv'))
-st.dataframe(df_c = pd.read_csv('constructors.csv'))
-st.dataframe(race_data = pd.read_csv('races.csv'))
+df_cr = pd.read_csv('constructor_results.csv'
+st.dataframe(df_cr)
+df_cs = pd.read_csv('constructor_standings.csv')
+st.dataframe(df_cs)                   
+df_c = pd.read_csv('constructors.csv')
+st.dataframe(df_c)
+race_data = pd.read_csv('races.csv')
+st.dataframe(race_data)
+Haas_id = df_c[df_c['constructorRef'] == 'haas']['constructorId'].values[0]
+st.write(Haas_id)
 
-st.write(Haas_id = df_c[df_c['constructorRef'] == 'haas']['constructorId'].values[0])
 
+df_cr_haas = df_cr[df_cr['constructorId'] == Haas_id]
+st.write(df_cr_haas)
+df_cs_haas = df_cs[df_cs['constructorId'] == Haas_id]
+st.write(df_cs_haas)
 
-st.write(df_cr_haas = df_cr[df_cr['constructorId'] == Haas_id])
-st.write(df_cs_haas = df_cs[df_cs['constructorId'] == Haas_id])
+Haas_construct = pd.merge(df_cr_haas, df_cs_haas, on=['raceId', 'constructorId'], suffixes=('_results', '_standings'))
 
-st.dataframe(Haas_construct = pd.merge(df_cr_haas, df_cs_haas, on=['raceId', 'constructorId'], suffixes=('_results', '_standings')))
+st.dataframe(Haas_construct)
 st.write(Haas_construct.head())
-st.dataframe(Haas_data = pd.merge(Haas_construct, race_data, on=['raceId']))
+Haas_data = pd.merge(Haas_construct, race_data, on=['raceId']
+st.dataframe(Haas_data)
 
-
-st.write(correlation_matrix = Haas_data[['points_results', 'points_standings', 'position', 'wins']].corr())
-
+correlation_matrix = Haas_data[['points_results', 'points_standings', 'position', 'wins']].corr()
+st.write(correlation_matrix)
 st.write(plt.figure(figsize=(8, 6)))
 st.write(sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm'))
 st.write(plt.title('Correlation Matrix of Performance Metrics for Haas'))

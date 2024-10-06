15cdb1abed6d18b6028081a0003780ff9b56e796
DDomogala3 <ddd38@nau.edu>
2024-10-05 19:53:16 -0500

    Update Formula_One_datavis_v0.py
    added csv imports

--- Formula_One_datavis_v0.py
+++ Formula_One_datavis_v0.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 #Formual One Streamlit script
 
 
-st.write("Formula One Data Analysis")
+st.write("Formula One Data Analysis:Coming July 2025")
+
+
+df_cr = pd.read_csv('/constructor_results.csv')
+df_cs = pd.read_csv('/constructor_standings.csv')
+df_c = pd.read_csv('/constructors.csv')
+race_data = pd.read_csv('/races.csv')
+
+Haas_id = df_c[df_c['constructorRef'] == 'haas']['constructorId'].values[0]
+
+
+
+

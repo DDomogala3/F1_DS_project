6e245f751ea4cb2cc4f03b840450205c982bea66
DDomogala3 <ddd38@nau.edu>
2024-10-05 19:54:03 -0500

    Update Formula_One_datavis_v0.py
    updated imports

--- Formula_One_datavis_v0.py
+++ Formula_One_datavis_v0.py
@@ -7,10 +7,10 @@ import numpy as np
 st.write("Formula One Data Analysis:Coming July 2025")
 
 
-df_cr = pd.read_csv('/constructor_results.csv')
-df_cs = pd.read_csv('/constructor_standings.csv')
-df_c = pd.read_csv('/constructors.csv')
-race_data = pd.read_csv('/races.csv')
+df_cr = pd.read_csv('constructor_results.csv')
+df_cs = pd.read_csv('constructor_standings.csv')
+df_c = pd.read_csv('constructors.csv')
+race_data = pd.read_csv('races.csv')
 
 Haas_id = df_c[df_c['constructorRef'] == 'haas']['constructorId'].values[0]
 

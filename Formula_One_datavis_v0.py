dab964bdbb5f61d212ac5389d34141eccfb53518
DDomogala3 <ddd38@nau.edu>
2024-10-19 13:56:38 -0500

    Update Formula_One_datavis_v0.py
    added lambda

--- Formula_One_datavis_v0.py
+++ Formula_One_datavis_v0.py
@@ -85,7 +85,7 @@ team_input_ds = df_ds[df_ds['driverId'].isin(team_driver_ids)]
 team_input_standings_with_names = pd.merge(team_input_results, team_input_drivers, on='driverId',how='left')
 haas_standings_with_names = pd.merge(haas_results, haas_drivers, on='driverId', how='left')
 
-#mclaren_standings_with_names = mclaren_standings_with_names[['driverId','forename', 'surname','points','position']]
+
 
 team_input_standings_with_names.rename(columns={'forename':'First Name','surname':'Last Name'}, inplace=True)
 
@@ -118,7 +118,9 @@ ax1.legend(loc='upper left')
 ax2.legend(loc='upper right')
 
 st.pyplot(plt)
+#race_data = pd.read_csv('/formula-1-world-championship-1950-2020/races.csv')
 
+result_by_year_lamda = lambda date,year,metric: data.groupby(year)[metric].mean().reset_index()
 
 def result_by_year(data, year, metric):
     avg = data.groupby(year)[metric].mean().reset_index()
@@ -139,65 +141,7 @@ plt.show()
 
 st.divider()
 
-st.header("Williams Racing Data Analysis")
-
-williams_id = df_c[df_c['constructorRef'] == 'williams']['constructorId'].values[0]
-
-df_cr_williams = df_cr[df_cr['constructorId'] == williams_id]
-df_cs_williams = df_cs[df_cs['constructorId'] == williams_id]
-
-williams_construct = pd.merge(df_cr_williams, df_cs_williams, on=['raceId', 'constructorId'], suffixes=('_results', '_standings'))
-williams_construct.head()
-williams_data = pd.merge(williams_construct, race_data, on=['raceId'])
-williams_data.head()
-
-correlation_matrix = williams_data[['points_results', 'points_standings', 'position', 'wins']].corr()
-
-plt.figure(figsize=(8, 6))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
-plt.title('Correlation Matrix of Performance Metrics for williams')
-st.pyplot(plt)
-
-st.write("Williams has had their ups and downs, but they have a storied history in Formula One")
-
-williams_results = df_results[df_results['constructorId'] == williams_id]
-
-williams_driver_ids = williams_results['driverId'].unique()
-
-williams_drivers = df_driv[df_driv['driverId'].isin(williams_driver_ids)]
-williams_ds = df_ds[df_ds['driverId'].isin(williams_driver_ids)]
 
 
-st.write(williams_drivers)
 
 
-williams_standings_with_names = pd.merge(williams_results, williams_drivers, on='driverId', how='left')
-
-#mclaren_standings_with_names = mclaren_standings_with_names[['driverId','forename', 'surname','points','position']]
-
-williams_standings_with_names.rename(columns={'forename': 'First Name', 'surname': 'Last Name'}, inplace=True)
-williams_standings_with_names['position'] = pd.to_numeric(williams_standings_with_names['position'], errors='coerce')
-
-williams_driver_performance = williams_standings_with_names.groupby(['Last Name'])[['points', 'position']].mean().reset_index()
-
-fig, ax1 = plt.subplots(figsize=(10,6))
-
-sns.barplot(data=williams_driver_performance, x='Last Name', y='points', ax=ax1, color='b', alpha=0.6, label='Average Points')
-ax1.set_xlabel('Driver')
-ax1.set_ylabel('Average Points', color='b')
-ax1.tick_params(axis='y', labelcolor='b')
-
-ax1.set_xticks(range(len(williams_driver_performance)))
-ax1.set_xticklabels(williams_driver_performance['Last Name'], rotation=90)
-
-ax2 = ax1.twinx()
-sns.lineplot(data=williams_driver_performance, x='Last Name', y='position', ax=ax2, color='r', marker='o', label='Average Position')
-
-ax2.set_ylabel('Average Position', color='r')
-ax2.tick_params(axis='y', labelcolor='r')
-
-plt.title('Average Points and Positions by Williams Driver')
-ax1.legend(loc='upper left')
-ax2.legend(loc='upper right')
-
-st.pyplot(plt)
